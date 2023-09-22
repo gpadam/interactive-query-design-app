755c5f7bbf53957c0a30ceda90fc435f09617f95
gaelen_adam@brown.edu <gaelen_adam@brown.edu>
2023-09-22 17:08:17 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    query = anvil.server.call('query_generator', 
+                                self.title.text,
+                                self.KQ.text)
+    if query:
+      self.query.visible = True
+      self.query = query()
